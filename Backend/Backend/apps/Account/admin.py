474b83f23d28f3c10adfe5f42db24fe4b4044739
Zhaofeng Liu <45281765+koking0@users.noreply.github.com>
2026-02-11 11:34:06 +0800

    Set custom titles for Django admin site
    Update Django admin site titles to reflect project name.

--- Backend/Backend/apps/Account/admin.py
+++ Backend/Backend/apps/Account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+
+# 修改网页 Title (浏览器标签页标题)
+admin.site.site_title = "{{ProjectChineseName}}-管理平台"
+# 修改登录页和首页页眉 (大的标题)
+admin.site.site_header = "{{ProjectChineseName}}-管理平台"
+# 修改首页标语 (首页中间标题)
+admin.site.index_title = "{{ProjectChineseName}}-管理平台"
