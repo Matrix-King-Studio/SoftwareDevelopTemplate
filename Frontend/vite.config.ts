cb4dcd94aee149f93e1ffd325f23baf707fad338
Zhaofeng Liu <45281765+koking0@users.noreply.github.com>
2026-02-15 04:53:38 +0000

    增加了环境配置信息

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: "http://127.0.0.1:8000/",
-        changeOrigin: true, //需要代理跨域
-        rewrite: (path) => path.replace(/^\/api/, ''), //路径重写，把'/api'替换为''
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src',
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true, //需要代理跨域
+          rewrite: (path) => path.replace(/^\/api/, ''), //路径重写，把'/api'替换为''
+        },
+      },
+    },
+  }
 })
